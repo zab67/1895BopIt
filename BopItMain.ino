c6c570692a92a73cc385b00c85b01ed71b6376f3
AnujLele <122324934+AnujLele@users.noreply.github.com>
2024-02-21 17:26:09 -0500

    Update BopItMain.ino

--- BopItMain.ino
+++ BopItMain.ino
@@ -1,14 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
+  time = maxTime;
 
 }
  
- static int max = 100;
+static int maxScore = 100;
+static float maxTime = 4;
+static float timeDecrement = 0.03;
+
 
 void loop() {
   int score = 0;
+  float time;
+
   
-  
+  time -= timeDecrement;
 }
 
 bool rageIt() {
