da04e47352b0ef13f3753dad1e1ba9136f04626d
KAP250 <122547052+KAP250@users.noreply.github.com>
2024-03-21 17:17:20 -0400

    Updated code with acc integrated into Slave

--- Master_Code.ino
+++ Master_Code.ino
@@ -1,10 +1,13 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(8,2); //RX, TX on pins RX to TX on HC-05 and TX to RX on HC-05
+SoftwareSerial BTSerial(10,9); //Setup for Master - RX, TX | for pins RX to TX on HC-05 and TX to RX on HC-05
 String bit = "0";
+int state = 0;
 void setup() 
 {
   Serial.begin(38400);
   BTSerial.begin(9600);
+
+  pinMode(8, OUTPUT);
 }
 
 void loop() 
@@ -14,4 +17,18 @@ void loop()
     bit = BTSerial.read();
     Serial.print(bit);
   }
+  if(bit == "49")
+  {
+    if(state == 0)
+    {
+      digitalWrite(8, HIGH);
+      state = 1;
+    }
+    else
+    {
+      digitalWrite(8, LOW);
+      state = 0;
+    }
+  }
+  bit = "";
 }
--- Slave_Code.ino
+++ Slave_Code.ino
@@ -1,16 +1,78 @@
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <MPU6050.h>
+MPU6050 mpu;
 SoftwareSerial BTSerial(10,9); //Setup for Slave - RX, TX | for pins RX to TX on HC-05 and TX to RX on HC-05
-
-void setup() {
-  // // put your setup code here, to run once:
+int buttonState = 0;  // variable for reading the pushbutton status
+void setup() 
+{
   Serial.begin(38400);
+
+  Serial.println("Initialize MPU6050");
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+  checkSettings();
+
   BTSerial.begin(9600);
-  pinMode(8, OUTPUT);
+  pinMode(8, INPUT);
 }
 
 void loop() 
 {
-  digitalWrite(8, HIGH);
-  BTSerial.write("1");
-  delay(1000);
+
+  Vector rawAccel = mpu.readRawAccel();
+  Vector normAccel = mpu.readNormalizeAccel();
+  if((normAccel.XAxis >= 1) or (normAccel.YAxis >= 1))
+  {
+    BTSerial.write("1");
+    delay(170);
+  }
+  Serial.print(normAccel.XAxis);
+  Serial.print(normAccel.YAxis);
+  Serial.println(normAccel.ZAxis);
+  delay(100);
+
+  buttonState = digitalRead(8);
+  if(buttonState == 1)
+  {
+    BTSerial.write("1");
+    delay(170);
+  }
+}
+
+void checkSettings()
+{
+  Serial.println();
+  Serial.print(" * Sleep Mode:            ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  Serial.print(" * Clock Source:          ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  Serial.print(" * Accelerometer:         ");
+  switch(mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }   
+  Serial.print(" * Accelerometer offsets: ");
+  Serial.print(mpu.getAccelOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getAccelOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getAccelOffsetZ());
+  
+  Serial.println();
 }
